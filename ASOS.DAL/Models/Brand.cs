a31ded8754f5e67e71a068a306c0ce94ee02cd8f
mahmoud adel <M777hmoued@gmail.com>
2025-04-03 17:31:00 +0200

    Edit Brand model

--- ASOS.DAL/Models/Brand.cs
+++ ASOS.DAL/Models/Brand.cs
@@ -4,7 +4,7 @@
     {
         public Guid Id { get; set; }
 
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         public string BrandImage { get; set; }
 
