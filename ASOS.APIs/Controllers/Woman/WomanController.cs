7c3646d3aa2c5de8647e2dc71499986b38325a48
mahmoud adel <M777hmoued@gmail.com>
2025-04-07 18:56:31 +0200

    Update all feature related to WomanController

--- ASOS.APIs/Controllers/Woman/WomanController.cs
+++ ASOS.APIs/Controllers/Woman/WomanController.cs
@@ -1,6 +1,7 @@
-﻿using ASOS.BL.DTOs;
-using ASOS.BL.Managers.Woman;
-using Microsoft.AspNetCore.Http;
+﻿using ASOS.BL;
+using ASOS.BL.DTOs;
+using ASOS.BL.DTOs.Common;
+using ASOS.BL.Managers.Category;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASOS.APIs.Controllers.Woman
@@ -10,16 +11,37 @@ namespace ASOS.APIs.Controllers.Woman
     public class WomanController : ControllerBase
     {
         private readonly IWomanManager _womanManager;
+        private readonly ICategoryManager _categoryManager;
 
-        public WomanController(IWomanManager womanManager)
+        public WomanController(IWomanManager womanManager ,ICategoryManager categoryManager)
         {
             _womanManager = womanManager;
+            _categoryManager = categoryManager;
         }
         [HttpGet]
-        public async Task<ActionResult<List<ProductDTO>>> GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            
-            return await _womanManager.GetAllAsync();
+            var products = await _womanManager.GetAllAsync();
+            return Ok(new GeneralResult<List<ProductDTO>>() { Data = products,Success = true, Errors = [] });
         }
+
+        [HttpGet("newin")]
+        public async Task<IActionResult> GetNewInWomenProducts()
+        {
+            var products = await _womanManager.GetAllAsync();
+            var newInProducts = products.OrderByDescending(p => p.CreatedAt).Take(16).ToList();
+
+            return Ok(new GeneralResult<List<ProductDTO>>() { Data = newInProducts, Success = true, Errors = [] });
+        }
+
+        [HttpGet("clothing")]
+        public async Task<IActionResult> GetClothingWomenProducts()
+        {
+            var products = await _womanManager.GetAllAsync();
+            var productsByCategory = products.Where(p => p.CategoryName == "Clothing").ToList();
+
+            return Ok(new GeneralResult<List<ProductDTO>>() { Data = productsByCategory, Success = true, Errors = [] });
+        }
+
     }
 }
