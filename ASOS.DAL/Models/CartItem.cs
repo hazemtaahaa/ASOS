9fbf8f31d2f894c4467c121a05309206ba151885
AbdullahEmad07 <abdullah.emad237@gmail.com>
2025-04-09 15:22:06 +0200

    update Cart and WishList

--- ASOS.DAL/Models/CartItem.cs
+++ ASOS.DAL/Models/CartItem.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ASOS.DAL.Models
+﻿namespace ASOS.DAL.Models
 {
-    public class CartItem
+	public class CartItem
     {   
         public Guid ProductId { get; set; }
 
         public Product? Product { get; set; }
 
-        public int Quantity { get; set; }
+        public int Quantity { get; set; } = 1;
 
         public Guid CartId { get; set; }
 
